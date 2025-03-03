333842c3f1c43424a78c75740fc20a5fed2974b5
Burak3745 <69092099+Burak3745@users.noreply.github.com>
2025-03-04 00:41:25 +0300

    first commit

---
+++ src/test/java/com/traflux/TrafluxAuthenticationService/TrafluxAuthenticationServiceTests.java
@@ -0,0 +1,13 @@
+package com.traflux.TrafluxAuthenticationService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrafluxAuthenticationServiceTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
